14708b67d7beacde5a64fbdefc009971927b0606
Serhey <deathwings9@gmail.com>
2017-09-25 13:02:51 +0300

    Update library.js

--- library.js
+++ library.js
@@ -71,50 +71,75 @@ exports.init = (params, next) => {
             db[method](set, min, max, start, stop - start, next)
           }
         },
-		function quickSort(items, left, right, next) {
-
-			  var index;
-
-			  if (items.length > 1) {
-
-				  left = typeof left != "number" ? 0 : left;
-				  right = typeof right != "number" ? items.length - 1 : right;
-
-				  index = partition(items, left, right);
-
-				  if (left < index - 1) {
-					  quickSort(items, left, index - 1);
-				  }
-
-				  if (index < right) {
-					  quickSort(items, index, right);
-				  }
-
-			  }
-
-			  next(null, items)
-
-			  return items;
+		function swap(items, firstIndex, secondIndex){
+			var temp = items[firstIndex];
+			items[firstIndex] = items[secondIndex];
+			items[secondIndex] = temp;
 		},
-        function (topicValues, next) {
+		function partition(items, left, right) {
+
+			var pivot   = items[Math.floor((right + left) / 2)],
+				i       = left,
+				j       = right;
+
+
+			while (i <= j) {
+
+				while (items[i] < pivot) {
+					i++;
+				}
+
+				while (items[j] > pivot) {
+					j--;
+				}
+
+				if (i <= j) {
+					swap(items, i, j);
+					i++;
+					j--;
+				}
+			}
+
+			return i;
+		},
+		function quickSort(items, left, right) {
+
+			var index;
+
+			if (items.length > 1) {
+
+				left = typeof left != "number" ? 0 : left;
+				right = typeof right != "number" ? items.length - 1 : right;
+
+				index = partition(items, left, right);
+
+				if (left < index - 1) {
+					quickSort(items, left, index - 1);
+				}
+
+				if (index < right) {
+					quickSort(items, index, right);
+				}
+
+			}
+
+			return items;
+		},
+		function (topicValues, next) {
           let tids = []
 
           topicValues.forEach(function (value) {
             tid = value.split(':')
             tid = tid[tid.length - 1]
             tids.push(tid)
-			  if (reverse) {
+			  if (!reverse) {
 				  quickSort(tids)
 			  } else {
-				  quickSort(tids)
+				  quickSort(tids).reverse()
 			  }
           })
 
           next(null, tids)
-
-
-
-		  tids.sort(function (a, b) {  return a - b;  })
 
           db.isSetMembers('sortbydate:purged', tids, function (err, isMember) {
             for (let i = 0; i < topicValues.length; i++) {
