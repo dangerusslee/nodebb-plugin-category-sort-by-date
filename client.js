614c79d29815c55045fc28fb37b78ed6cf59a5ca
Serhey <deathwings9@gmail.com>
2017-11-06 15:43:37 +0200

    Update client.js

--- client.js
+++ client.js
@@ -1,5 +1,5 @@
 $(window).on('action:ajaxify.end', function () {
   var sortEl = $('.category [component="thread/sort"] ul');
-  sortEl.append('<li><a href="#" class="topics_oldest_to_newest" data-sort="topics_oldest_to_newest"><i class="fa fa-fw ' + (config.categoryTopicSort === 'topics_oldest_to_newest' ? 'fa-check' : '') + '"></i> Topics Newest to Oldest</a></li>');  
-  sortEl.append('<li><a href="#" class="topics_newest_to_oldest" data-sort="topics_newest_to_oldest"><i class="fa fa-fw ' + (config.categoryTopicSort === 'topics_newest_to_oldest' ? 'fa-check' : '') + '"></i> Topics Oldest to Newest</a></li>');
+  sortEl.append('<li><a href="#" class="topics_oldest_to_newest" data-sort="topics_oldest_to_newest"><i class="fa fa-fw ' + (config.categoryTopicSort === 'topics_oldest_to_newest' ? 'fa-check' : '') + '"></i> Начиная с новых тем</a></li>');  
+  sortEl.append('<li><a href="#" class="topics_newest_to_oldest" data-sort="topics_newest_to_oldest"><i class="fa fa-fw ' + (config.categoryTopicSort === 'topics_newest_to_oldest' ? 'fa-check' : '') + '"></i> Начиная со старых тем</a></li>');
 });
