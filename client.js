6e6da4042422be1ee94d85bef349aa328b9d2a8a
DangerRuss <dangeruss@phatwalletforums.com>
2017-11-11 22:12:12 -0500

    adding Russian translation

--- client.js
+++ client.js
@@ -1,5 +1,11 @@
 $(window).on('action:ajaxify.end', function () {
-  var sortEl = $('.category [component="thread/sort"] ul');
-  sortEl.append('<li><a href="#" class="topics_oldest_to_newest" data-sort="topics_oldest_to_newest"><i class="fa fa-fw ' + (config.categoryTopicSort === 'topics_oldest_to_newest' ? 'fa-check' : '') + '"></i> Topics Newest to Oldest</a></li>');  
-  sortEl.append('<li><a href="#" class="topics_newest_to_oldest" data-sort="topics_newest_to_oldest"><i class="fa fa-fw ' + (config.categoryTopicSort === 'topics_newest_to_oldest' ? 'fa-check' : '') + '"></i> Topics Oldest to Newest</a></li>');
+  require(['translator'], function(translator) {
+    translator.translate('[[topic:topics_newest_to_oldest]]', function(newest_to_oldest) { 
+      translator.translate('[[topic:topics_oldest_to_newest]]', function(oldest_to_newest) { 
+        var sortEl = $('.category [component="thread/sort"] ul');
+        sortEl.append('<li><a href="#" class="topics_oldest_to_newest" data-sort="topics_oldest_to_newest"><i class="fa fa-fw ' + (config.categoryTopicSort === 'topics_oldest_to_newest' ? 'fa-check' : '') + '"></i> ' + newest_to_oldest + '</a></li>');  
+        sortEl.append('<li><a href="#" class="topics_newest_to_oldest" data-sort="topics_newest_to_oldest"><i class="fa fa-fw ' + (config.categoryTopicSort === 'topics_newest_to_oldest' ? 'fa-check' : '') + '"></i> ' + oldest_to_newest + '</a></li>');
+      });
+    });
+  });
 });
